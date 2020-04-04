09663ac0464469e4e1f62fd72bbc953a70dd3c88
Rounak Vyas <itsron143@gmail.com>
2020-04-03 22:51:26 -0400

    this might be it

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -5,8 +5,8 @@ generate(){
         $(rm rounakvyas.pdf)
     fi
     $(./laton cv_7.tex > /dev/null 2>&1)
-    $(ls)
-    $(mv main.pdf pdf/rounakvyas.pdf)
+    $(sleep 5)
+    $(mv cv_7.pdf pdf/rounakvyas.pdf)
 }
 
 generate
