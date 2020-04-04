ba8297eeffeae5a47a246c8d9a048a43a598d726
Rounak Vyas <itsron143@gmail.com>
2020-04-03 22:46:57 -0400

    flush stdout

--- scripts/generate_pdf.sh
+++ scripts/generate_pdf.sh
@@ -4,7 +4,8 @@ generate(){
     if [ -f pdf/rounakvyas.pdf ]; then
         $(rm rounakvyas.pdf)
     fi
-    $(./laton cv_7.tex)
+    $(./laton cv_7.tex > /dev/null 2>&1)
+    $(sleep 5)
     $(mv main.pdf pdf/rounakvyas.pdf)
 }
 
